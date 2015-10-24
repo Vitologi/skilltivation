cb4595108e6cfad24c5a3ca76fa2367d0bffcde9
Spektr <spektrasm@yandex.ru>
2015-10-25 02:13:20 +0300

    add sources templates

---
+++ client/views/source/source_list.js
@@ -0,0 +1,22 @@
+var tempSourcesData = [
+    {
+        "title":"Совершенный код",
+        "authors":[
+            {
+                "name":"Макконел"
+            }
+        ]
+    },
+    {
+        "title":"JavaScript подробное руководство",
+        "authors":[
+            {
+                "name":"Фленеган"
+            }
+        ]
+    }
+];
+
+Template.sourceList.helpers({
+    "sources":tempSourcesData
+});
