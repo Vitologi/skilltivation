48648aaea03f53e9b27d8c7a82c76a6397d63da8
Spektr <spektrasm@yandex.ru>
2015-11-01 15:08:20 +0300

    create field for authors and tags

--- client/views/source/source_edit.js
+++ client/views/source/source_edit.js
@@ -1,7 +1,54 @@
 Template.sourceEdit.onRendered(function(){
     $('.source-edit-choser')
-        .tooltip()
-        .collapse();
+        .tooltip();
+});
+
+Template.sourceEdit.onRendered(function () {
+
+    // Save open panels
+    if(Session.get("showAuthor")){
+        Session.set("showAuthor", null);
+        $("a[href=#source-edit-authors]").click();
+    }
+    if(Session.get("showTags")){
+        Session.set("showTags", null);
+        $("a[href=#source-edit-tags]").click();
+    }
+
+    // Material tags (add plugin)
+    $('#source-edit-authors input').tagsinput();
+    $('#source-edit-tags input').tagsinput();
+
+
+});
+
+
+Template.sourceEdit.helpers({
+    "showAuthor":function(){
+        return Session.get("showAuthor");
+    },
+
+    "showTags":function(){
+        return Session.get("showTags");
+    },
+
+    "existAuthors":function(){
+        return [
+            {"_id":1, "name":"Фленеган"},
+            {"_id":2, "name":"Кроксфорд"},
+            {"_id":3, "name":"Шопэн"}
+        ];
+        return Session.get("authors");
+    },
+
+    "existTags":function(){
+        return [
+            {"_id":1, "name":"css"},
+            {"_id":2, "name":"javascript"},
+            {"_id":3, "name":"html"}
+        ];
+        return Session.get("authors");
+    }
 });
 
 Template.sourceEdit.events({
@@ -18,8 +65,18 @@ Template.sourceEdit.events({
             if (error)
                 return alert(error.reason);
         });
+    },
+
+    "click a[href=#source-edit-authors]":function(){
+        Session.set("showAuthor", !Session.get("showAuthor"));
+    },
+
+    "click a[href=#source-edit-tags]":function(){
+        Session.set("showTags", !Session.get("showTags"));
     }
 });
+
+
 
 
 
