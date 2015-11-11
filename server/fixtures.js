2ace39c7cf232f4b8f28e76bf5bc58881d4bfb72
Spektr <spektrasm@yandex.ru>
2015-11-11 23:49:24 +0300

    publish and subscribe collections, set default values

--- server/fixtures.js
+++ server/fixtures.js
@@ -5,3 +5,20 @@ if(Types.find().count() === 0){
     Types.insert({title:"Видео"});
     Types.insert({title:"Аудио"});
 }
+
+// Insert authors
+if(Authors.find().count() === 0){
+    Authors.insert({name:"Дарья Донцова"});
+    Authors.insert({name:"Стивен Кинг"});
+    Authors.insert({name:"Рэй Бредбери"});
+}
+
+// Insert tags
+if(Tags.find().count() === 0){
+    Tags.insert({title:"Css"});
+    Tags.insert({title:"JavaScript"});
+    Tags.insert({title:"PHP"});
+    Tags.insert({title:"Детектив"});
+    Tags.insert({title:"Фэнтези"});
+    Tags.insert({title:"Фантастика"});
+}
