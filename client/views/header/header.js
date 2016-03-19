be048eccc38682d4e04bfc259f8e5562c558da3d
Spektr <spektr1987@rambler.ru>
2016-03-20 00:48:02 +0300

    change registration bundle and i18n it

--- client/views/header/header.js
+++ client/views/header/header.js
@@ -1,1 +1,16 @@
+Template.header.onRendered(function(){
 
+    // [hack] Synchronize languages TODO: fix this
+    TAPi18n._afterUILanguageChange = function(){
+        accountsUIBootstrap3.setLanguage(TAPi18n.getLanguage());
+    };
+    TAPi18n._afterUILanguageChange();
+
+
+});
+
+Template.header.events({
+    "click .header__options select":function(event){
+        event.stopPropagation();
+    }
+});
